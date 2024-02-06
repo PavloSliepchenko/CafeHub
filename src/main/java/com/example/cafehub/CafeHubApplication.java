1f4a6c3174feb9610627da8ca42190b45eb4f816
PavloSliepchenko <sliepchenkopavlo@gmail.com>
2024-02-06 03:25:12 -0600

    first commit

---
+++ src/main/java/com/example/cafehub/CafeHubApplication.java
@@ -0,0 +1,11 @@
+package com.example.cafehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeHubApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CafeHubApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/cafehub/CafeHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cafehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
