1c581a11fbf9819b8e0c83c8afb00f7450dca746
PavloSliepchenko <sliepchenkopavlo@gmail.com>
2024-03-11 17:23:49 -0500

    Added a check for the presence of an adding cafe

--- src/main/java/com/example/cafehub/repository/CafeRepository.java
+++ src/main/java/com/example/cafehub/repository/CafeRepository.java
@@ -2,10 +2,13 @@ package com.example.cafehub.repository;
 
 import com.example.cafehub.model.Cafe;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface CafeRepository extends JpaRepository<Cafe, Long>, JpaSpecificationExecutor<Cafe> {
     List<Cafe> findByCity(String city, Pageable pageable);
+
+    Optional<Cafe> findByNameAndAddressAndCity(String name, String address, String city);
 }
