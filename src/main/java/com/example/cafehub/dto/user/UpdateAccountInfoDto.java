31f182a059f24c46f9f9fa30a3609545fa04f80d
PavloSliepchenko <sliepchenkopavlo@gmail.com>
2024-05-10 16:50:20 -0500

    Added email pattern

--- src/main/java/com/example/cafehub/dto/user/UpdateAccountInfoDto.java
+++ src/main/java/com/example/cafehub/dto/user/UpdateAccountInfoDto.java
@@ -3,12 +3,14 @@ package com.example.cafehub.dto.user;
 import com.example.cafehub.validator.ValidFirstCapitalLetter;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
 @Data
 public class UpdateAccountInfoDto {
-    @Email
+    @Email(regexp = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}",
+            flags = Pattern.Flag.CASE_INSENSITIVE)
     @NotNull
     @Length(max = 32)
     private String email;
